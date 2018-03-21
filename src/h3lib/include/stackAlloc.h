9a1ac18ff996dc7a84b407141c2fb376802520e2
Isaac Hier <isaachier@gmail.com>
2018-03-21 19:40:12 -0400

    Make sure stack allocations are not empty

--- src/h3lib/include/stackAlloc.h
+++ src/h3lib/include/stackAlloc.h
@@ -1,13 +1,15 @@
 #ifndef STACKALLOC_H
 #define STACKALLOC_H
 
+#include <assert.h>
 #include <string.h>
 
 #ifdef H3_HAVE_VLA
 
-#define STACK_ARRAY_CALLOC(type, name, numElements) \
-    type name##Buffer[(numElements)];               \
-    memset(name##Buffer, 0, sizeof(name##Buffer));  \
+#define STACK_ARRAY_CALLOC(type, name, numElements)        \
+    assert((numElements) > 0);                             \
+    type name##Buffer[(numElements)];                      \
+    memset(name##Buffer, 0, (numElements) * sizeof(type)); \
     type* name = name##Buffer
 
 #elif defined(H3_HAVE_ALLOCA)
@@ -17,6 +19,7 @@
 #include <malloc.h>
 
 #define STACK_ARRAY_CALLOC(type, name, numElements)            \
+    assert((numElements) > 0);                                 \
     type* name = (type*)_alloca(sizeof(type) * (numElements)); \
     memset(name, 0, sizeof(type) * (numElements))
 
@@ -25,6 +28,7 @@
 #include <alloca.h>
 
 #define STACK_ARRAY_CALLOC(type, name, numElements)           \
+    assert((numElements) > 0);                                \
     type* name = (type*)alloca(sizeof(type) * (numElements)); \
     memset(name, 0, sizeof(type) * (numElements))
 
