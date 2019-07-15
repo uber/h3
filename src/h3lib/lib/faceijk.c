1df5c3473ad4ce585ecca5278088189612c98cee
Nicholas Rabinowitz <nickr@uber.com>
2019-07-15 09:50:41 -0700

    Update copyright years

--- src/h3lib/lib/faceijk.c
+++ src/h3lib/lib/faceijk.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 Uber Technologies, Inc.
+ * Copyright 2016-2019 Uber Technologies, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
