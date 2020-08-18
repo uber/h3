3a898b8c542d2805413135f692d791e6c6564ba1
AJ Friend <ajfriend@gmail.com>
2020-08-17 20:38:04 -0700

    formatting happened

--- src/apps/testapps/testH3UniEdgeExhaustive.c
+++ src/apps/testapps/testH3UniEdgeExhaustive.c
@@ -38,8 +38,7 @@ static void h3UniEdge_correctness_assertions(H3Index h3) {
 
     for (int i = 0; i < 6; i++) {
         if (isPentagon && i == 0) {
-            t_assert(edges[i] == H3_NULL,
-                     "last pentagon edge is empty");
+            t_assert(edges[i] == H3_NULL, "last pentagon edge is empty");
             continue;
         }
         t_assert(H3_EXPORT(h3UnidirectionalEdgeIsValid)(edges[i]) == 1,
