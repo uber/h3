af74f2179415f47fe66e9307ef72192d821ea9a3
AJ Friend <ajfriend@gmail.com>
2020-09-08 09:26:20 -0700

    valgring bugfix attempt #2

--- src/apps/testapps/testGeoCoord.c
+++ src/apps/testapps/testGeoCoord.c
@@ -236,8 +236,7 @@ SUITE(geoCoord) {
         for (int i = 0; i < N; i++) {
             H3_EXPORT(getH3UnidirectionalEdgesFromHexagon)(cells[i], edges);
 
-            for (int j = 0; H3_EXPORT(h3UnidirectionalEdgeIsValid)(edges[j]);
-                 j++) {
+            for (int j = 0; j < 5; j++) {
                 double len = H3_EXPORT(exactEdgeLengthM)(edges[j]);
                 t_assert(len > 0, "edge has positive length");
             }
