14fefac940d6bbe202e035eb6365274abb1ab765
AJ Friend <ajfriend@gmail.com>
2020-09-09 23:29:02 -0700

    add some test documentation

--- src/apps/testapps/testH3CellAreaExhaustive.c
+++ src/apps/testapps/testH3CellAreaExhaustive.c
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /** @file
  * @brief tests H3 cell area functions using tests over a large number
  *        of indexes.
@@ -26,33 +27,19 @@
 #include "test.h"
 #include "utility.h"
 
-static void h3UniEdge_length_assertions(H3Index edge) {
-    char msg[] = "edge has positive length";
-
-    t_assert(H3_EXPORT(exactEdgeLengthRads)(edge) > 0, msg);
-    t_assert(H3_EXPORT(exactEdgeLengthKm)(edge) > 0, msg);
-    t_assert(H3_EXPORT(exactEdgeLengthM)(edge) > 0, msg);
-}
-
-static void cell_area_assertions(H3Index cell) {
-    char msg[] = "cell has positive length";
-
-    t_assert(H3_EXPORT(cellAreaRads2)(cell) > 0, msg);
-    t_assert(H3_EXPORT(cellAreaKm2)(cell) > 0, msg);
-    t_assert(H3_EXPORT(cellAreaM2)(cell) > 0, msg);
-}
-
-static void earthAreaTest(int res, double (*callback)(H3Index), double target,
-                          double tol) {
-    double area = mapSumAllCells_double(res, callback);
-
-    t_assert(fabs(area - target) < tol,
-             "sum of all cells should give earth area");
-}
-
-static void commutative_distance_assertions(H3Index edge) {
+/**
+ * Basic checks around the great circle distance between the centers of two
+ * neighboring cells. Tests positivity and commutativity.
+ *
+ * Tests the functions:
+ *     pointDistRads
+ *     pointDistKm
+ *     pointDistM
+ *
+ * @param  edge  H3 unidirectional edge denoting neighboring cells
+ */
+static void haversine_assert(H3Index edge) {
     GeoCoord a, b;
-    double ab, ba;
 
     H3_EXPORT(h3ToGeo)
     (H3_EXPORT(getOriginH3IndexFromUnidirectionalEdge)(edge), &a);
@@ -61,6 +48,8 @@ static void commutative_distance_assertions(H3Index edge) {
 
     char pos[] = "distance between cell centers should be positive";
     char comm[] = "pairwise cell distances should be commutative";
+
+    double ab, ba;
 
     ab = H3_EXPORT(pointDistRads)(&a, &b);
     ba = H3_EXPORT(pointDistRads)(&b, &a);
@@ -78,22 +67,80 @@ static void commutative_distance_assertions(H3Index edge) {
     t_assert(ab == ba, comm);
 }
 
+/**
+ * Tests positivity of edge length calculation for the functions:
+ *
+ *      exactEdgeLengthRads
+ *      exactEdgeLengthKm
+ *      exactEdgeLengthM
+ *
+ * @param  edge  edge to compute the length of
+ */
+static void edge_length_assert(H3Index edge) {
+    char msg[] = "edge has positive length";
+
+    t_assert(H3_EXPORT(exactEdgeLengthRads)(edge) > 0, msg);
+    t_assert(H3_EXPORT(exactEdgeLengthKm)(edge) > 0, msg);
+    t_assert(H3_EXPORT(exactEdgeLengthM)(edge) > 0, msg);
+}
+
+/**
+ * Test that cell area calculations are positive for the functions:
+ *
+ *      cellAreaRads2
+ *      cellAreaKm2
+ *      cellAreaM2
+ *
+ * @param  cell  cell to compute the area of
+ */
+static void cell_area_assert(H3Index cell) {
+    char msg[] = "cell has positive length";
+
+    t_assert(H3_EXPORT(cellAreaRads2)(cell) > 0, msg);
+    t_assert(H3_EXPORT(cellAreaKm2)(cell) > 0, msg);
+    t_assert(H3_EXPORT(cellAreaM2)(cell) > 0, msg);
+}
+
+/**
+ * Apply a cell area calculation function to every cell on the earth at a given
+ * resolution, and check that it sums up the total earth area.
+ *
+ * @param  res        resolution of the cells
+ * @param  cell_area  callback to compute area of each cell
+ * @param  target     expected earth area in some units
+ * @param  tol        error tolerance allowed between expected and actual
+ */
+static void earth_area_test(int res, double (*cell_area)(H3Index),
+                            double target, double tol) {
+    double area = mapSumAllCells_double(res, cell_area);
+
+    t_assert(fabs(area - target) < tol,
+             "sum of all cells should give earth area");
+}
+
 SUITE(h3CellArea) {
-    TEST(h3UniEdge_length) {
-        iterateAllUnidirectionalEdgesAtRes(0, h3UniEdge_length_assertions);
-        iterateAllUnidirectionalEdgesAtRes(1, h3UniEdge_length_assertions);
-        iterateAllUnidirectionalEdgesAtRes(2, h3UniEdge_length_assertions);
-        iterateAllUnidirectionalEdgesAtRes(3, h3UniEdge_length_assertions);
+    TEST(haversine_distances) {
+        iterateAllUnidirectionalEdgesAtRes(0, haversine_assert);
+        iterateAllUnidirectionalEdgesAtRes(1, haversine_assert);
+        iterateAllUnidirectionalEdgesAtRes(2, haversine_assert);
+        iterateAllUnidirectionalEdgesAtRes(3, haversine_assert);
     }
 
-    TEST(cellAreaPositive) {
-        iterateAllIndexesAtRes(0, cell_area_assertions);
-        iterateAllIndexesAtRes(1, cell_area_assertions);
-        iterateAllIndexesAtRes(2, cell_area_assertions);
-        iterateAllIndexesAtRes(3, cell_area_assertions);
+    TEST(edge_length) {
+        iterateAllUnidirectionalEdgesAtRes(0, edge_length_assert);
+        iterateAllUnidirectionalEdgesAtRes(1, edge_length_assert);
+        iterateAllUnidirectionalEdgesAtRes(2, edge_length_assert);
+        iterateAllUnidirectionalEdgesAtRes(3, edge_length_assert);
     }
 
-    TEST(cellAreaEarth) {
+    TEST(cell_area_positive) {
+        iterateAllIndexesAtRes(0, cell_area_assert);
+        iterateAllIndexesAtRes(1, cell_area_assert);
+        iterateAllIndexesAtRes(2, cell_area_assert);
+        iterateAllIndexesAtRes(3, cell_area_assert);
+    }
+
+    TEST(cell_area_earth) {
         // earth area in different units
         double rads2 = 4 * M_PI;
         double km2 = rads2 * EARTH_RADIUS_KM * EARTH_RADIUS_KM;
@@ -103,31 +150,24 @@ SUITE(h3CellArea) {
         // I think this has something to do with Class II vs Class III
         // resolutions.
 
-        earthAreaTest(0, H3_EXPORT(cellAreaRads2), rads2, 1e-14);
-        earthAreaTest(0, H3_EXPORT(cellAreaKm2), km2, 1e-6);
-        earthAreaTest(0, H3_EXPORT(cellAreaM2), m2, 1e0);
+        earth_area_test(0, H3_EXPORT(cellAreaRads2), rads2, 1e-14);
+        earth_area_test(0, H3_EXPORT(cellAreaKm2), km2, 1e-6);
+        earth_area_test(0, H3_EXPORT(cellAreaM2), m2, 1e0);
 
-        earthAreaTest(1, H3_EXPORT(cellAreaRads2), rads2, 1e-9);
-        earthAreaTest(1, H3_EXPORT(cellAreaKm2), km2, 1e-1);
-        earthAreaTest(1, H3_EXPORT(cellAreaM2), m2, 1e5);
+        earth_area_test(1, H3_EXPORT(cellAreaRads2), rads2, 1e-9);
+        earth_area_test(1, H3_EXPORT(cellAreaKm2), km2, 1e-1);
+        earth_area_test(1, H3_EXPORT(cellAreaM2), m2, 1e5);
 
-        earthAreaTest(2, H3_EXPORT(cellAreaRads2), rads2, 1e-12);
-        earthAreaTest(2, H3_EXPORT(cellAreaKm2), km2, 1e-5);
-        earthAreaTest(2, H3_EXPORT(cellAreaM2), m2, 1e0);
+        earth_area_test(2, H3_EXPORT(cellAreaRads2), rads2, 1e-12);
+        earth_area_test(2, H3_EXPORT(cellAreaKm2), km2, 1e-5);
+        earth_area_test(2, H3_EXPORT(cellAreaM2), m2, 1e0);
 
-        earthAreaTest(3, H3_EXPORT(cellAreaRads2), rads2, 1e-11);
-        earthAreaTest(3, H3_EXPORT(cellAreaKm2), km2, 1e-3);
-        earthAreaTest(3, H3_EXPORT(cellAreaM2), m2, 1e3);
+        earth_area_test(3, H3_EXPORT(cellAreaRads2), rads2, 1e-11);
+        earth_area_test(3, H3_EXPORT(cellAreaKm2), km2, 1e-3);
+        earth_area_test(3, H3_EXPORT(cellAreaM2), m2, 1e3);
 
-        earthAreaTest(4, H3_EXPORT(cellAreaRads2), rads2, 1e-11);
-        earthAreaTest(4, H3_EXPORT(cellAreaKm2), km2, 1e-3);
-        earthAreaTest(4, H3_EXPORT(cellAreaM2), m2, 1e2);
-    }
-
-    TEST(commutative_distances) {
-        iterateAllUnidirectionalEdgesAtRes(0, commutative_distance_assertions);
-        iterateAllUnidirectionalEdgesAtRes(1, commutative_distance_assertions);
-        iterateAllUnidirectionalEdgesAtRes(2, commutative_distance_assertions);
-        iterateAllUnidirectionalEdgesAtRes(3, commutative_distance_assertions);
+        earth_area_test(4, H3_EXPORT(cellAreaRads2), rads2, 1e-11);
+        earth_area_test(4, H3_EXPORT(cellAreaKm2), km2, 1e-3);
+        earth_area_test(4, H3_EXPORT(cellAreaM2), m2, 1e2);
     }
 }
