e9cf21b755db2bd839511aed91f8a5e4882b1a54
Isaac Brodsky <isaac@isaacbrodsky.com>
2021-02-15 20:20:24 -0800

    Add fuzzer harnesses

---
+++ src/apps/fuzzers/fuzzerGeoToH3.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/** @file
+ * @brief Fuzzer program for geoToH3
+ */
+
+#include "h3api.h"
+#include "utility.h"
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        error("Should have one argument (test case file)\n");
+    }
+    const char* filename = argv[1];
+    FILE* fp = fopen(filename, "rb");
+    struct args {
+        double lat;
+        double lon;
+        int res;
+    } args;
+    if (fread(&args, sizeof(args), 1, fp) != 1) {
+        error("Error reading\n");
+    }
+    fclose(fp);
+
+    GeoCoord g = {.lat = args.lat,
+                  .lon = args.lon};
+    H3Index h = H3_EXPORT(geoToH3)(&g, args.res);
+
+    h3Println(h);
+}
---
+++ src/apps/fuzzers/fuzzerKRing.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/** @file
+ * @brief Fuzzer program for kRing
+ */
+
+#include "h3api.h"
+#include "utility.h"
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        error("Should have one argument (test case file)\n");
+    }
+    const char* filename = argv[1];
+    FILE* fp = fopen(filename, "rb");
+    struct {
+        H3Index index;
+        int k;
+    } args;
+    if (fread(&args, sizeof(args), 1, fp) != 1) {
+        error("Error reading\n");
+    }
+    fclose(fp);
+
+    int sz = H3_EXPORT(maxKringSize)(args.k);
+    H3Index* results = calloc(sizeof(H3Index), sz);
+    if (results != NULL) {
+         H3_EXPORT(kRing)(args.index, args.k, results);
+         h3Println(results[0]);
+    }
+    free(results);
+}
