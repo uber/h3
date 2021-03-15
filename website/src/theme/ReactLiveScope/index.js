2341ae782ac60c90ae57dc6cb5134ffb9a5b5e20
Nick Rabinowitz <nick@unfolded.ai>
2021-03-15 15:50:22 -0700

    Fix the build by using the h3-js browser bundle and shimming browser globals

--- website/src/theme/ReactLiveScope/index.js
+++ website/src/theme/ReactLiveScope/index.js
@@ -1,11 +1,11 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+// It's not clear why this dance is necessary, but without it
+// the build fails due to "document" references in h3-js. If we
+// import the UMD bundle for h3-js instead, the build fails on
+// "Buffer" references.
 
-const h3 = require("h3-js");
+const window = require("global/window");
+window.document ||= require("global/document");
+const h3 = require("h3-js/dist/browser/h3-js.js");
 
 // Add react-live imports you need here
 const ReactLiveScope = {
