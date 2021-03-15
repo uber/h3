e281f75341f831afcef87238769c17c7383f64fa
Nick Rabinowitz <nick@unfolded.ai>
2021-03-15 16:12:25 -0700

    Fix dev website build

--- website/src/theme/ReactLiveScope/index.js
+++ website/src/theme/ReactLiveScope/index.js
@@ -1,16 +1,18 @@
-// It's not clear why this dance is necessary, but without it
-// the build fails due to "document" references in h3-js. If we
-// import the UMD bundle for h3-js instead, the build fails on
-// "Buffer" references.
+/**
+ * It's not clear why this dance is necessary, but without it
+ * the build fails due to "document" references in h3-js. If we
+ * import the UMD bundle for h3-js instead, the build fails on
+ * "Buffer" references...
+ */
 
-const window = require("global/window");
-window.document ||= require("global/document");
-const h3 = require("h3-js/dist/browser/h3-js.js");
+if (typeof window === 'undefined' || !window.document) {
+  const window = require("global/window");
+  window.document = window.document || require("global/document");
+}
 
 // Add react-live imports you need here
 const ReactLiveScope = {
-  h3,
-  ...h3,
+  h3: require("h3-js/dist/browser/h3-js.js")
 };
 
 export default ReactLiveScope;
