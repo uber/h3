c57342903bd21f74be9de216c5e3a2796d0ec490
Isaac Brodsky <isaac@unfolded.ai>
2021-03-16 10:31:30 -0700

    Fix capitalization of doc ID

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     'Getting Started': ['README', 'usecases', 'installation', 'quickstart'],
-    'Core Library': ['core-library/overview', 'core-library/h3indexing', 'core-library/coordsystems', 'core-library/geoToH3desc', 'core-library/h3ToGeoDesc', 'core-library/h3ToGeoBoundaryDesc', 'core-library/filters', 'core-library/usage', 'core-library/restable'],
+    'Core Library': ['core-library/overview', 'core-library/h3Indexing', 'core-library/coordsystems', 'core-library/geoToH3desc', 'core-library/h3ToGeoDesc', 'core-library/h3ToGeoBoundaryDesc', 'core-library/filters', 'core-library/usage', 'core-library/restable'],
     'API Reference': ['api/indexing', 'api/inspection', 'api/traversal', 'api/hierarchy', 'api/regions', 'api/uniedge', 'api/misc'],
     'Community': ['community/bindings', 'community/libraries', 'community/tutorials', 'community/applications'],
   },
