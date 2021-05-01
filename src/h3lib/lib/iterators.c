9506f684b03fbece2614920414a8b84bd53612ad
AJ Friend <ajfriend@gmail.com>
2021-05-01 16:10:07 -0400

    spacing

--- src/h3lib/lib/iterators.c
+++ src/h3lib/lib/iterators.c
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /** @file Iter_Child.c
  * @brief Iterator structs and functions for the children of a cell,
  * or cells at a given resolution.
