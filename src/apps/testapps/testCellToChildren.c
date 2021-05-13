9eba7989b45d1f687886d38954b9aef16f717baa
AJ Friend <ajfriend@gmail.com>
2021-05-13 00:30:21 -0700

    testCellToChildren file comment

--- src/apps/testapps/testCellToChildren.c
+++ src/apps/testapps/testCellToChildren.c
@@ -13,6 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/** @file
+ * @brief tests H3 functions `cellToChildren` and `cellToChildrenSize`
+ *
+ *  usage: `testCellToChildren`
+ */
 
 #include <stdlib.h>
 
