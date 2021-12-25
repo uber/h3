3eed379d6e909eeb55ed9ea7ed445872ca46e59d
AJ Friend <ajfriend@gmail.com>
2021-12-25 18:21:56 -0500

    clean up helper functions

--- src/apps/testapps/testLow52.c
+++ src/apps/testapps/testLow52.c
@@ -21,8 +21,6 @@
  *  usage: `testLow52`
  */
 
-#include <math.h>
-
 #include "h3api.h"
 #include "test.h"
 
@@ -31,44 +29,44 @@ typedef struct {
     int64_t N;
 } CellArray;
 
-CellArray initCellArray(int64_t N) {
+CellArray ca_init(int64_t N) {
     CellArray a = {.cells = calloc(N, sizeof(H3Index)), .N = N};
 
     return a;
 }
 
-CellArray getDisk(H3Index h, int k) {
-    CellArray arr = initCellArray(H3_EXPORT(maxGridDiskSize)(k));
+CellArray ca_disk(H3Index h, int k) {
+    CellArray arr = ca_init(H3_EXPORT(maxGridDiskSize)(k));
     H3_EXPORT(gridDisk)(h, k, arr.cells);
 
     return arr;
 }
 
-CellArray getRing(H3Index h, int k) {
-    CellArray A = initCellArray(6 * k);
+CellArray ca_ring(H3Index h, int k) {
+    CellArray A = ca_init(6 * k);
     H3_EXPORT(gridRingUnsafe)(h, k, A.cells);
 
     return A;
 }
 
-void doCanon(CellArray *arr) {
+void ca_canon(CellArray *arr) {
     int64_t N;
     canonicalizeCells(arr->cells, arr->N, &N);
     arr->N = N;
 }
 
-CellArray doCompact(CellArray arr) {
-    CellArray packed = initCellArray(arr.N);
+CellArray ca_compact(CellArray arr) {
+    CellArray packed = ca_init(arr.N);
     H3_EXPORT(compactCells)(arr.cells, packed.cells, arr.N);
 
     return packed;
 }
 
-CellArray doUncompact(CellArray arr, int res) {
+CellArray ca_uncompact(CellArray arr, int res) {
     int64_t N;
     H3_EXPORT(uncompactCellsSize)(arr.cells, arr.N, res, &N);
 
-    CellArray out = initCellArray(N);
+    CellArray out = ca_init(N);
     H3_EXPORT(uncompactCells)(arr.cells, arr.N, out.cells, out.N, res);
 
     return out;
@@ -78,6 +76,10 @@ void t_intersect(CellArray A, CellArray B, bool result) {
     t_assert(result == intersectTheyDo(A.cells, A.N, B.cells, B.N), "");
 }
 
+void t_contains(CellArray A, H3Index h, bool result) {
+    t_assert(result == canonSearch(A.cells, A.N, h), "");
+}
+
 void t_isLow52(CellArray A, bool result) {
     t_assert(result == isLow52Sorted(A.cells, A.N), "");
 }
@@ -85,12 +87,13 @@ void t_isCanon(CellArray A, bool result) {
     t_assert(result == isCanonicalCells(A.cells, A.N), "");
 }
 
-void diskIntersect(H3Index a, H3Index b, int ka, int kb, bool shouldIntersect) {
-    CellArray A = getDisk(a, ka);
-    CellArray B = getDisk(b, kb);
+void t_diskIntersect(H3Index a, H3Index b, int ka, int kb,
+                     bool shouldIntersect) {
+    CellArray A = ca_disk(a, ka);
+    CellArray B = ca_disk(b, kb);
 
-    doCanon(&A);
-    doCanon(&B);
+    ca_canon(&A);
+    ca_canon(&B);
 
     t_intersect(A, B, shouldIntersect);
     t_intersect(B, A, shouldIntersect);
@@ -99,17 +102,17 @@ void diskIntersect(H3Index a, H3Index b, int ka, int kb, bool shouldIntersect) {
     free(B.cells);
 }
 
-void diskIntersectCompact(H3Index a, H3Index b, int ka, int kb,
-                          bool shouldIntersect) {
+void t_diskIntersectCompact(H3Index a, H3Index b, int ka, int kb,
+                            bool shouldIntersect) {
     CellArray A, B, cA, cB;
 
-    A = getDisk(a, ka);
-    B = getDisk(b, kb);
+    A = ca_disk(a, ka);
+    B = ca_disk(b, kb);
 
-    cA = doCompact(A);
-    cB = doCompact(B);
-    doCanon(&cA);
-    doCanon(&cB);
+    cA = ca_compact(A);
+    cB = ca_compact(B);
+    ca_canon(&cA);
+    ca_canon(&cB);
 
     t_intersect(cA, cB, shouldIntersect);
     t_intersect(cB, cA, shouldIntersect);
@@ -120,15 +123,13 @@ void diskIntersectCompact(H3Index a, H3Index b, int ka, int kb,
     free(cB.cells);
 }
 
-// add empty input tests
-// uncompact of a canonical set should give you a canonical set
-
 SUITE(low52tests) {
     TEST(basic_low52) {
         H3Index h = 0x89283082e73ffff;
         int k = 100;
 
-        CellArray A = getDisk(h, k);
+        CellArray A = ca_disk(h, k);
+        CellArray Z = {.N = 0, .cells = NULL};  // empty cell array
 
         // low 52 tests
         t_isLow52(A, false);  // shouldn't be sorted yet
@@ -136,18 +137,16 @@ SUITE(low52tests) {
         t_isLow52(A, true);  // should be sorted now!
 
         // canonical tests
-        t_isCanon(A, true);  // No duplicates, so should already be canon
+        t_isCanon(A, true);  // no duplicates, so should already be canon
         int64_t numBefore = A.N;
-        doCanon(&A);
+        ca_canon(&A);
         t_assert(A.N == numBefore, "Expect no change from canonicalizing.");
 
         // binary search
-        t_assert(canonSearch(A.cells, A.N, h), "Needs to be in there!");
-        t_assert(!canonSearch(A.cells, 0, h), "h can't be in empty set.");
+        t_contains(A, h, true);   // Needs to be in there!
+        t_contains(Z, h, false);  // h can't be in an empty set
 
         // intersection
-        CellArray Z = {.N = 0, .cells = NULL};  // empty cell array
-
         t_intersect(A, A, true);
         t_intersect(Z, A, false);  // first is empty
         t_intersect(A, Z, false);  // second is empty
@@ -160,10 +159,10 @@ SUITE(low52tests) {
         H3Index h = 0x89283082e73ffff;
         int k = 100;
 
-        CellArray A = getDisk(h, k);
+        CellArray A = ca_disk(h, k);
 
         int64_t numBefore = A.N;
-        doCanon(&A);
+        ca_canon(&A);
         t_assert(A.N == numBefore, "Expect no change from canonicalizing.");
 
         t_isLow52(A, true);
@@ -176,7 +175,7 @@ SUITE(low52tests) {
         t_isCanon(A, false);
 
         // canonicalizing again should remove the zero
-        doCanon(&A);
+        ca_canon(&A);
         t_assert(A.N == numBefore - 1, "Lose one cell.");
         t_isCanon(A, true);
 
@@ -188,22 +187,22 @@ SUITE(low52tests) {
         int res = 9;
         int k = 100;
 
-        CellArray u = getDisk(h, k);  // uncompacted set
-        doCanon(&u);
+        CellArray u = ca_disk(h, k);  // uncompacted set
+        ca_canon(&u);
 
-        CellArray c = doCompact(u);  // compacted set
-        doCanon(&c);
+        CellArray c = ca_compact(u);  // compacted set
+        ca_canon(&c);
 
         t_isCanon(u, true);
         t_isCanon(c, true);
 
-        t_assert(canonSearch(c.cells, c.N, h), "");
+        t_contains(c, h, true);
         t_intersect(c, c, true);
         t_intersect(c, u, true);
         t_intersect(u, c, true);
 
         // test that uncompact keeps things canonical
-        CellArray u2 = doUncompact(c, res);
+        CellArray u2 = ca_uncompact(c, res);
         t_isCanon(u2, true);
 
         free(c.cells);
@@ -215,15 +214,14 @@ SUITE(low52tests) {
         H3Index h = 0x89283082e73ffff;
         int k = 10;
 
-        CellArray A = getRing(h, k);
-        CellArray B = getRing(h, k + 1);
-        doCanon(&A);
-        doCanon(&B);
+        CellArray A = ca_ring(h, k);
+        CellArray B = ca_ring(h, k + 1);
+        ca_canon(&A);
+        ca_canon(&B);
 
-        t_assert(!canonSearch(A.cells, A.N, h), "");
-        t_assert(!canonSearch(B.cells, B.N, h), "");
-
-        t_assert(canonSearch(A.cells, A.N, A.cells[0]), "");
+        t_contains(A, h, false);
+        t_contains(B, h, false);
+        t_contains(A, A.cells[0], true);
 
         t_intersect(A, B, false);
         t_intersect(B, A, false);
@@ -232,7 +230,7 @@ SUITE(low52tests) {
 
         // add a cell from A to B, so they now intersect
         B.cells[B.N / 2] = A.cells[A.N / 2];
-        doCanon(&B);
+        ca_canon(&B);
         t_intersect(A, B, true);
         t_intersect(A, B, true);
 
@@ -249,15 +247,15 @@ SUITE(low52tests) {
         t_assert(k == 20, "");
 
         // not compacted
-        diskIntersect(a, b, 9, 9, false);   // not yet
-        diskIntersect(a, b, 9, 10, false);  // just barely disjoint
-        diskIntersect(a, b, 10, 10, true);  // overlap
-        diskIntersect(a, b, 11, 11, true);  // more overlap
+        t_diskIntersect(a, b, 9, 9, false);   // not yet
+        t_diskIntersect(a, b, 9, 10, false);  // just barely disjoint
+        t_diskIntersect(a, b, 10, 10, true);  // overlap
+        t_diskIntersect(a, b, 11, 11, true);  // more overlap
 
         // compacted
-        diskIntersectCompact(a, b, 9, 9, false);   // not yet
-        diskIntersectCompact(a, b, 9, 10, false);  // just barely disjoint
-        diskIntersectCompact(a, b, 10, 10, true);  // overlap
-        diskIntersectCompact(a, b, 11, 11, true);  // more overlap
+        t_diskIntersectCompact(a, b, 9, 9, false);   // not yet
+        t_diskIntersectCompact(a, b, 9, 10, false);  // just barely disjoint
+        t_diskIntersectCompact(a, b, 10, 10, true);  // overlap
+        t_diskIntersectCompact(a, b, 11, 11, true);  // more overlap
     }
 }
