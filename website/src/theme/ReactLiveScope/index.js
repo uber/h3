6c77803da8d4b7669ac6d07bb91ba75243f20b44
Isaac Brodsky <isaac@isaacbrodsky.com>
2022-08-03 15:19:28 -0700

    Update website to 4.0.0-rc1 (#634)
    * Update website to RC1
    
    * do not alias main version of h3-js

--- website/src/theme/ReactLiveScope/index.js
+++ website/src/theme/ReactLiveScope/index.js
@@ -12,7 +12,8 @@ if (typeof window === 'undefined' || !window.document) {
 
 // Add react-live imports you need here
 const ReactLiveScope = {
-  h3: require("h3-js/dist/browser/h3-js.js")
+  h3: require("h3-js/dist/browser/h3-js.js"),
+  h3v3: require("h3-jsv3/dist/browser/h3-js.js")
 };
 
 export default ReactLiveScope;
