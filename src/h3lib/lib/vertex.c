961ea595db0ac780076e297e997654817c255ff1
Isaac Brodsky <isaac@isaacbrodsky.com>
2022-08-09 10:57:19 -0700

    Code coverage improvement for vertex functions (#637)
    * Code coverage improvement for vertex functions
    
    * remove incorrect comment

--- src/h3lib/lib/vertex.c
+++ src/h3lib/lib/vertex.c
@@ -221,8 +221,7 @@ H3Error H3_EXPORT(cellToVertex)(H3Index cell, int vertexNum, H3Index *out) {
     if (res == 0 || H3_GET_INDEX_DIGIT(cell, res) != CENTER_DIGIT) {
         // Get the left neighbor of the vertex, with its rotations
         Direction left = directionForVertexNum(cell, vertexNum);
-        // This case should be unreachable; invalid verts fail earlier
-        if (left == INVALID_DIGIT) return E_FAILED;  // LCOV_EXCL_LINE
+        if (left == INVALID_DIGIT) return E_FAILED;
         int lRotations = 0;
         H3Index leftNeighbor;
         h3NeighborRotations(cell, left, &lRotations, &leftNeighbor);
