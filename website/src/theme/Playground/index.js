b4bdc4feaddc40693678416ec87d438669cdb282
Isaac Brodsky <isaac@unfolded.ai>
2022-08-25 15:10:43 -0700

    Upgrade website to latest Docusaurus

--- website/src/theme/Playground/index.js
+++ website/src/theme/Playground/index.js
@@ -1,22 +1,12 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-import * as React from "react";
-import { LiveProvider, LiveEditor, LiveError, LiveContext } from "react-live";
-import clsx from "clsx";
-import Translate from "@docusaurus/Translate";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import React from 'react';
+import clsx from 'clsx';
 import useIsBrowser from '@docusaurus/useIsBrowser';
-import usePrismTheme from "@theme/hooks/usePrismTheme";
-import styles from "./styles.module.css";
-
-function Header({ children }) {
-  return <div className={clsx(styles.playgroundHeader)}>{children}</div>;
-}
+import {LiveProvider, LiveEditor, LiveError, LivePreview, LiveContext} from 'react-live';
+import Translate from '@docusaurus/Translate';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import {usePrismTheme} from '@docusaurus/theme-common';
+import styles from './styles.module.css';
 
 // Modified version to wrap:
 // https://github.com/FormidableLabs/react-live/blob/4ef370647d4e52f8f87148b70c6429237de508cb/src/utils/transpile/errorBoundary.js
@@ -30,77 +20,84 @@ class ErrorBoundary2 extends React.Component {
   render() {
     return this.props.children;
   }
-};
+}
 
+function Header({children}) {
+  return <div className={clsx(styles.playgroundHeader)}>{children}</div>;
+}
+function LivePreviewLoader() {
+  // Is it worth improving/translating?
+  // eslint-disable-next-line @docusaurus/no-untranslated-text
+  return <div>Loading...</div>;
+}
 function ResultWithHeader() {
   return (
     <>
       <Header>
         <Translate
           id="theme.Playground.result"
-          description="The result label of the live codeblocks"
-        >
+          description="The result label of the live codeblocks">
           Result
         </Translate>
       </Header>
+      {/* https://github.com/facebook/docusaurus/issues/5747 */}
       <div className={styles.playgroundPreview}>
-        {/* This div replaces LivePreview */}
-        <LiveContext.Consumer>
-          {({ element: Element }) =>
-            Element ? (
-              <pre>
-                <ErrorBoundary2>
-                  <Element />
-                </ErrorBoundary2>
-              </pre>
-            ) : null
-          }
-        </LiveContext.Consumer>
-        <LiveError />
+        <BrowserOnly fallback={<LivePreviewLoader />}>
+          {() => (
+            <>
+              <LivePreview Component='code' />
+              <LiveError />
+            </>
+          )}
+        </BrowserOnly>
       </div>
     </>
   );
 }
-
+function ThemedLiveEditor() {
+  const isBrowser = useIsBrowser();
+  return (
+    <LiveEditor
+      // We force remount the editor on hydration,
+      // otherwise dark prism theme is not applied
+      key={String(isBrowser)}
+      className={styles.playgroundEditor}
+    />
+  );
+}
 function EditorWithHeader() {
   return (
     <>
       <Header>
         <Translate
           id="theme.Playground.liveEditor"
-          description="The live editor label of the live codeblocks"
-        >
+          description="The live editor label of the live codeblocks">
           Live Editor
         </Translate>
       </Header>
-      <LiveEditor className={styles.playgroundEditor} />
+      <ThemedLiveEditor />
     </>
   );
 }
-
-export default function Playground({ children, transformCode, ...props }) {
-  const isClient = useIsBrowser();
+export default function Playground({children, transformCode, ...props}) {
   const {
-    siteConfig: {
-      themeConfig: {
-        liveCodeBlock: { playgroundPosition },
-      },
-    },
+    siteConfig: {themeConfig},
   } = useDocusaurusContext();
+  const {
+    liveCodeBlock: {playgroundPosition},
+  } = themeConfig;
   const prismTheme = usePrismTheme();
-
+  const noInline = props.metastring?.includes('noInline') ?? false;
   return (
     <div className={styles.playgroundContainer}>
+      {/* @ts-expect-error: type incompatibility with refs */}
       <LiveProvider
-        key={isClient}
-        code={isClient ? children.replace(/\n$/, "") : ""}
-        transformCode={
-          transformCode || ((code) => `JSON.stringify(${code}());`)
-        }
+        code={children.replace(/\n$/, '')}
+        noInline={noInline}
+        transformCode={transformCode || ((code) => `JSON.stringify(${code}(), null, 2) || 'undefined';`)}
         theme={prismTheme}
-        {...props}
-      >
-        {playgroundPosition === "top" ? (
+        {...props}>
+        {playgroundPosition === 'top' ? (
           <>
             <ResultWithHeader />
             <EditorWithHeader />
