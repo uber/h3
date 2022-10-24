6e2157716516a605ded85010ddadc630815e8866
Isaac Brodsky <isaac@isaacbrodsky.com>
2022-10-24 15:01:30 -0400

    Add ALWAYS macro from Sqlite

--- src/h3lib/include/alloc.h
+++ src/h3lib/include/alloc.h
@@ -24,7 +24,7 @@
 #ifndef ALLOC_H
 #define ALLOC_H
 
-#include "h3api.h" // for TJOIN
+#include "h3api.h"  // for TJOIN
 
 #ifdef H3_ALLOC_PREFIX
 #define H3_MEMORY(name) TJOIN(H3_ALLOC_PREFIX, name)
@@ -33,10 +33,10 @@
 extern "C" {
 #endif
 
-void* H3_MEMORY(malloc)(size_t size);
-void* H3_MEMORY(calloc)(size_t num, size_t size);
-void* H3_MEMORY(realloc)(void* ptr, size_t size);
-void H3_MEMORY(free)(void* ptr);
+void *H3_MEMORY(malloc)(size_t size);
+void *H3_MEMORY(calloc)(size_t num, size_t size);
+void *H3_MEMORY(realloc)(void *ptr, size_t size);
+void H3_MEMORY(free)(void *ptr);
 
 #ifdef __cplusplus
 }
---
+++ src/h3lib/include/h3Assert.h
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2022 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/** @file h3Assert.h
+ * @brief   Assertion definitions
+ *
+ * TODO
+ * Adapted from https://www.sqlite.org/testing.html and
+ * https://www.sqlite.org/assert.html
+ *
+ * Used under the terms of the SQLite3 project, reproduced below:
+ * The author disclaims copyright to this source code.  In place of
+ * a legal notice, here is a blessing:
+ *
+ *    May you do good and not evil.
+ *    May you find forgiveness for yourself and forgive others.
+ *    May you share freely, never taking more than you give.
+ */
+
+#ifndef H3ASSERT_H
+#define H3ASSERT_H
+
+/*
+** The testcase() macro is used to aid in coverage testing.  When
+** doing coverage testing, the condition inside the argument to
+** testcase() must be evaluated both true and false in order to
+** get full branch coverage.  The testcase() macro is inserted
+** to help ensure adequate test coverage in places where simple
+** condition/decision coverage is inadequate.  For example, testcase()
+** can be used to make sure boundary values are tested.  For
+** bitmask tests, testcase() can be used to make sure each bit
+** is significant and used at least once.  On switch statements
+** where multiple cases go to the same block of code, testcase()
+** can insure that all cases are evaluated.
+*/
+#if defined(H3_COVERAGE_TEST) || defined(H3_DEBUG)
+extern unsigned int h3CoverageCounter;
+#define testcase(X)                              \
+    if (X) {                                     \
+        h3CoverageCounter += (unsigned)__LINE__; \
+    }
+#else
+#define testcase(X)
+#endif
+
+/*
+** Disable ALWAYS() and NEVER() (make them pass-throughs) for coverage
+** and mutation testing
+*/
+#if defined(H3_COVERAGE_TEST)
+#define H3_OMIT_AUXILIARY_SAFETY_CHECKS 1
+#endif
+
+/*
+** The ALWAYS and NEVER macros surround boolean expressions which
+** are intended to always be true or false, respectively.  Such
+** expressions could be omitted from the code completely.  But they
+** are included in a few cases in order to enhance the resilience
+** of SQLite to unexpected behavior - to make the code "self-healing"
+** or "ductile" rather than being "brittle" and crashing at the first
+** hint of unplanned behavior.
+**
+** In other words, ALWAYS and NEVER are added for defensive code.
+**
+** When doing coverage testing ALWAYS and NEVER are hard-coded to
+** be true and false so that the unreachable code they specify will
+** not be counted as untested code.
+*/
+#if defined(H3_OMIT_AUXILIARY_SAFETY_CHECKS)
+#define ALWAYS(X) (1)
+#define NEVER(X) (0)
+#elif !defined(NDEBUG)
+#define ALWAYS(X) ((X) ? 1 : (assert(0), 0))
+#define NEVER(X) ((X) ? (assert(0), 1) : 0)
+#else
+#define ALWAYS(X) (X)
+#define NEVER(X) (X)
+#endif
+
+#endif
