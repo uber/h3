5f6ede964bcc3fe92c497e9b0e2d9940eb5c36ea
Nick Rabinowitz <nick@unfolded.ai>
2023-09-29 17:34:23 +0800

    First pass at polyfill algo, untested

--- src/h3lib/include/polyfill.h
+++ src/h3lib/include/polyfill.h
@@ -25,6 +25,44 @@
 #include "h3api.h"
 #include "bbox.h"
 
+/**
+ * IterCellsPolygonCompact: struct for iterating through all the cells
+ * within a given polygon, outputting a compact set.
+ *
+ * Constructors:
+ *
+ * Initialize with `iterInitPolygonCompact`. This will save a reference to the
+ * input polygon and allocate memory for data structures used in the
+ * iteration. Iterators initialized in this way must be destroyed by
+ * `iterDestroyPolygon` to free allocated memory.
+ *
+ * Iteration:
+ *
+ * Step iterator with `iterStepPolygonCompact`.
+ * During the lifetime of the `iterCellsPolygonCompact`, the current iterate
+ * is accessed via the `iterCellsPolygonCompact.cell` member. When the iterator is 
+ * exhausted  or if there was an error in initialization or iteration, 
+ * `iterCellsPolygonCompact.cell` will be `H3_NULL` after calling `iterStepChild`. 
+ * It is the responsibiliy of the caller to check  `iterCellsPolygonCompact.error`
+ * when `H3_NULL` is received.
+ *
+ * Cleanup:
+ *
+ * Destroy the iterator and free allocated memory with `iterDestroyPolygonCompact`.
+ */
+typedef struct {
+    H3Index cell; // current value
+    H3Error error; // error, if any
+    int _res;  // target resolution
+    uint32_t _flags; // Mode flags for the polygonToCells operation
+    GeoPolygon *_polygon; // the polygon we're filling
+    BBox *_bboxes; // Bounding box(es) for the polygon and its holes
+} IterCellsPolygonCompact;
+
+DECLSPEC IterCellsPolygonCompact iterInitPolygonCompact(GeoPolygon *polygon, int res, uint32_t flags);
+DECLSPEC void iterStepPolygonCompact(IterCellsPolygonCompact *iter);
+DECLSPEC void iterDestroyPolygonCompact(IterCellsPolygonCompact *iter);
+
 H3Error cellToBBox(H3Index cell, BBox *out, bool coverChildren);
 
 #endif
--- src/h3lib/lib/polyfill.c
+++ src/h3lib/lib/polyfill.c
@@ -21,6 +21,8 @@
 
 #include <string.h>
 
+#include "alloc.h"
+#include "coordijk.h"
 #include "h3Assert.h"
 #include "h3Index.h"
 #include "polygon.h"
@@ -95,4 +97,215 @@ H3Error cellToBBox(H3Index cell, BBox *out, bool coverChildren) {
     }
 
     return E_SUCCESS;
+}
+
+/**
+ * Get a base cell by number, or H3_NULL if out of bounds
+ */
+static H3Index getBaseCell(int baseCellNum) {
+    if (baseCellNum < 0 || baseCellNum >= NUM_BASE_CELLS) {
+        return H3_NULL;
+    }
+    H3Index baseCell;
+    setH3Index(&baseCell, 0, baseCellNum, 0);
+    return baseCell;
+}
+
+static void iterErrorPolygonCompact(IterCellsPolygonCompact *iter,
+                                    H3Error error) {
+    iterDestroyPolygonCompact(iter);
+    iter->error = error;
+}
+
+/**
+ * Given a cell, find the next cell in the sequence of all cells
+ * to check in the iteration.
+ */
+static H3Index nextCell(H3Index cell) {
+    int res = H3_GET_RESOLUTION(cell);
+    while (true) {
+        // If this is a base cell, set to next base cell (or H3_NULL if done)
+        if (res == 0) {
+            return getBaseCell(H3_GET_BASE_CELL(cell) + 1);
+        }
+        // If not the last sibling of parent, return next sibling
+        Direction digit = H3_GET_INDEX_DIGIT(cell, res);
+        if (digit < INVALID_DIGIT) {
+            H3_SET_INDEX_DIGIT(
+                cell, res,
+                digit + (H3_EXPORT(isPentagon)(cell) && digit == CENTER_DIGIT
+                             ? 2  // Skip missing pentagon child
+                             : 1));
+            return cell;
+        }
+        // Move up to the parent for the next loop iteration
+        res--;
+        H3Index parent;
+        H3Error err = H3_EXPORT(cellToParent)(cell, res, &parent);
+        if (NEVER(err != E_SUCCESS)) {
+            // Should be unreachable; res-- can't be out of range, and can't
+            // mismatch current
+            return H3_NULL;
+        }
+        cell = parent;
+    }
+}
+
+/**
+ * Initialize a IterCellsPolygonCompact struct representing the sequence of
+ * cells within the target polygon. The test for including edge cells is defined
+ * by the polyfill mode passed in the `flags` argument.
+ *
+ * Initialization of this object may fail, in which case the `error` property
+ * will be set and all iteration will return H3_NULL. It is the responsibility
+ * of the caller to check the error property after initialization.
+ *
+ * At any point in the iteration, starting once the struct is initialized, the
+ * output value can be accessed through the `cell` property.
+ *
+ * Note that initializing the iterator allocates memory. If an iterator is
+ * exhausted or returns an error that memory is released; otherwise it must be
+ * released manually with iterDestroyPolygonCompact.
+ *
+ * @param  polygon Polygon to fill with compact cells
+ * @param  res     Finest resolution for output cells
+ * @param  flags   Bit mask of option flags
+ * @return         Initialized iterator, with the first value available
+ */
+IterCellsPolygonCompact iterInitPolygonCompact(GeoPolygon *polygon, int res,
+                                               uint32_t flags) {
+    IterCellsPolygonCompact iter = {
+        // Initialize output properties. The first cell will be set in iterStep
+        .cell = getBaseCell(0),
+        .error = E_SUCCESS,
+        // Save input arguments
+        ._polygon = polygon,
+        ._res = res,
+        ._flags = flags,
+        ._bboxes = NULL};
+
+    if (res < 0 || res > MAX_H3_RES) {
+        iterErrorPolygonCompact(&iter, E_RES_DOMAIN);
+        return iter;
+    }
+
+    if (flags != 0) {
+        iterErrorPolygonCompact(&iter, E_OPTION_INVALID);
+        return iter;
+    }
+
+    // Initialize bounding boxes for polygon and any holes. Memory allocated
+    // here must be released through iterDestroyPolygonCompact
+    iter._bboxes = H3_MEMORY(malloc)((polygon->numHoles + 1) * sizeof(BBox));
+    if (!iter._bboxes) {
+        iterErrorPolygonCompact(&iter, E_MEMORY_ALLOC);
+        return iter;
+    }
+    bboxesFromGeoPolygon(polygon, iter._bboxes);
+
+    return iter;
+}
+
+/**
+ * Increment the polyfill iterator, running the polygon to cells algorithm.
+ *
+ * Briefly, the algorithm checks every cell in the global grid hierarchically,
+ * starting with the base cells. Cells coarser than the target resolution are
+ * checked for complete child inclusion using a bounding box guaranteed to
+ * contain all children.
+ * - If the bounding box is contained by the polygon, output is set to the cell
+ * - If the bounding box intersects, recurse into the first child
+ * - Otherwise, continue with the next cell in sequence
+ *
+ * For cells at the target resolution, a finer-grained check is used according
+ * to the inclusion criteria set in flags.
+ *
+ * @param  iter Iterator to increment
+ * @return      Updated iterator, with the latest value in the `cell` property
+ */
+void iterStepPolygonCompact(IterCellsPolygonCompact *iter) {
+    H3Index cell = iter->cell;
+    H3Error err;
+
+    // once cell == H3_NULL, the iterator returns an infinite sequence of
+    // H3_NULL
+    if (cell == H3_NULL) return;
+
+    while (cell) {
+        int cellRes = H3_GET_RESOLUTION(cell);
+
+        // Target res: Do a fine-grained check
+        if (cellRes == iter->_res) {
+            // Check if the cell is in the polygon
+            // TODO: Handle other polyfill modes here
+            LatLng center;
+            H3_EXPORT(cellToLatLng)(cell, &center);
+            if (pointInsidePolygon(iter->_polygon, iter->_bboxes, &center)) {
+                // Set to next output
+                iter->cell = cell;
+                return;
+            }
+        }
+
+        // Coarser cell: Check the bounding box
+        if (cellRes < iter->_res) {
+            // Get a bounding box for all of the cell's children
+            BBox bbox;
+            err = cellToBBox(cell, &bbox, true);
+            if (err) {
+                iterErrorPolygonCompact(iter, err);
+                return;
+            }
+            if (bboxIntersects(&bbox, &iter->_bboxes[0])) {
+                // Convert bbox to cell boundary, CCW vertex order
+                CellBoundary bboxBoundary = {
+                    .numVerts = 4,
+                    .verts = {{bbox.north, bbox.east},
+                              {bbox.north, bbox.west},
+                              {bbox.south, bbox.west},
+                              {bbox.south, bbox.east}}};
+                if (cellBoundaryInsidePolygon(iter->_polygon, iter->_bboxes,
+                                              &bboxBoundary, &bbox)) {
+                    // Bounding box is fully contained, so all children are
+                    // included. Set to next output.
+                    iter->cell = cell;
+                    return;
+                }
+                // Otherwise, the intersecting bbox means we need to test all
+                // children, starting with the first child
+                H3Index child;
+                err = H3_EXPORT(cellToCenterChild)(cell, cellRes + 1, &child);
+                if (err) {
+                    iterErrorPolygonCompact(iter, err);
+                    return;
+                }
+                // Restart the loop with the child cell
+                cell = child;
+                continue;
+            }
+        }
+
+        // Find the next cell in the sequence of all cells and continue
+        cell = nextCell(cell);
+    }
+    // If we make it out of the loop, we're done
+    iterDestroyPolygonCompact(iter);
+}
+
+/**
+ * Destroy an iterator, releasing any allocated memory. Iterators destroyed in
+ * this manner are safe to use but will always return H3_NULL.
+ * @param  iter Iterator to destroy
+ * @return      [description]
+ */
+void iterDestroyPolygonCompact(IterCellsPolygonCompact *iter) {
+    if (iter->_bboxes) {
+        H3_MEMORY(free)(iter->_bboxes);
+    }
+    iter->cell = H3_NULL;
+    iter->error = E_SUCCESS;
+    iter->_polygon = NULL;
+    iter->_res = -1;
+    iter->_flags = 0;
+    iter->_bboxes = NULL;
 }
