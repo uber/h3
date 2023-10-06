135e5a3acd285193cb591930756bdb15626b8f1b
Nick Rabinowitz <public@nickrabinowitz.com>
2023-10-06 09:52:25 -0700

    Copyright fix
    Co-authored-by: Isaac Brodsky <isaac@isaacbrodsky.com>

--- src/h3lib/include/polyfill.h
+++ src/h3lib/include/polyfill.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017, 2020-2021 Uber Technologies, Inc.
+ * Copyright 2023 Uber Technologies, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
