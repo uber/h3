af425567076bc80aae8761ea7ced42a147776e21
Nick Rabinowitz <nick@unfolded.ai>
2023-10-31 15:54:59 -0700

    Benchmark old/new against each other

--- scripts/make_countries.js
+++ scripts/make_countries.js
@@ -60,10 +60,12 @@ function formatGeoLoop(loop) {
   }`
 }
 
-function formatGeoPolygon(poly) {
+function formatGeoPolygon(poly, {i, name, verts}) {
   const holes = poly.slice(1);
 
-  return `{
+  return `
+  // ${i}. ${name} (${verts} verts)
+  {
     .geoloop = ${formatGeoLoop(poly[0])},
     .numHoles = ${holes.length}${holes.length ? `,
     .holes = (GeoLoop[]) {
@@ -98,59 +100,64 @@ async function makeCountries(sourceUrl, targetPath) {
 
   console.log(`Found ${polygons.length} polygons`);
 
+  // In order to only allocate memory once, we take the biggest size
+  // at res 7, pad it, and size up based on powers of 7. This number was
+  // empirically determined.
+  const MAX_SIZE_RES_7 = 17544338;
+
   const out = `/*
-   * Copyright 2023 Uber Technologies, Inc.
-   *
-   * Licensed under the Apache License, Version 2.0 (the "License");
-   * you may not use this file except in compliance with the License.
-   * You may obtain a copy of the License at
-   *
-   *         http://www.apache.org/licenses/LICENSE-2.0
-   *
-   * Unless required by applicable law or agreed to in writing, software
-   * distributed under the License is distributed on an "AS IS" BASIS,
-   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   * See the License for the specific language governing permissions and
-   * limitations under the License.
-   */
-  
-  #include "benchmark.h"
-  #include "h3api.h"
-  #include "polyfill.h"
+ * Copyright 2023 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-  /*
-  Country names associated with each polygon:
-  ${
-    names.map(({name, i, verts}) => `${i}. ${name} (${verts} verts)`).join('\n  ')
-  }
-   */
+#include "benchmark.h"
+#include "h3api.h"
+#include "mathExtensions.h"
+#include "polyfill.h"
 
-  const GeoPolygon COUNTRIES[${polygons.length}] = {${
-    polygons.map(formatGeoPolygon).join(',')
-  }};
+const GeoPolygon COUNTRIES[${polygons.length}] = {${
+  polygons.map((poly, i) => formatGeoPolygon(poly, names[i])).join(',')
+}};
 
-  BEGIN_BENCHMARKS();
-  
-  
-  int64_t numHexagons;
-  H3Index *hexagons;
-  
-  for (int res = 0; res < 8; res++) {
+BEGIN_BENCHMARKS();
 
-    printf("Res %d", res);
-  
-    BENCHMARK(polygonToCells_AllCountries, 5, {
-      for (int index = 0; index < ${polygons.length}; index++) {
-        H3_EXPORT(maxPolygonToCellsSize)(&COUNTRIES[index], res, 0, &numHexagons);
-        hexagons = calloc(numHexagons, sizeof(H3Index));
-        H3_EXPORT(polygonToCells)(&COUNTRIES[index], res, 0, hexagons);
-        free(hexagons);
-      }
-    });
-  
-  }
-  
-  END_BENCHMARKS();
+int MAX_RES = 5;
+// Max size of largest polygon at res 5, rounded up with padding
+int64_t numHexagons = 400000;
+H3Index *hexagons = calloc(numHexagons, sizeof(H3Index));
+
+for (int res = 0; res < MAX_RES + 1; res++) {
+
+  printf("Res %d", res);
+
+  BENCHMARK(polygonToCells_AllCountries, 5, {
+    for (int index = 0; index < ${polygons.length}; index++) {
+      H3_EXPORT(polygonToCells)(&COUNTRIES[index], res, 0, hexagons);
+    }
+  });
+
+  BENCHMARK(polygonToCells_AllCountries, 5, {
+    for (int index = 0; index < ${polygons.length}; index++) {
+      H3_EXPORT(polygonToCellsExperimental)(&COUNTRIES[index], res, 0, hexagons);
+    }
+  });
+
+}
+
+free(hexagons);
+
+END_BENCHMARKS();
   `
 
   const outPath = path.join(__dirname, '..', targetPath);
