4f0341cf5b5cf992af5cb8c541ad1d56b9a21a59
Isaac Brodsky <isaac@isaacbrodsky.com>
2023-12-15 10:59:34 -0800

    guard against numVerts = 0 for hole check

--- src/h3lib/lib/polygon.c
+++ src/h3lib/lib/polygon.c
@@ -130,11 +130,12 @@ bool cellBoundaryInsidePolygon(const GeoPolygon *geoPolygon, const BBox *bboxes,
 
     // Check for line intersections with, or containment of, any hole
     for (int i = 0; i < geoPolygon->numHoles; i++) {
-        // TODO: There may be numVerts = 0 here, which would cause a crash
-        if (pointInsideGeoLoop(&boundaryLoop, boundaryBBox,
-                               &geoPolygon->holes[i].verts[0]) ||
-            cellBoundaryCrossesGeoLoop(&(geoPolygon->holes[i]), &bboxes[i + 1],
-                                       boundary, boundaryBBox)) {
+        // If the hole has no verts, it is not possible to intersect with it.
+        if (geoPolygon->holes[i].numVerts > 0 &&
+            (pointInsideGeoLoop(&boundaryLoop, boundaryBBox,
+                                &geoPolygon->holes[i].verts[0]) ||
+             cellBoundaryCrossesGeoLoop(&(geoPolygon->holes[i]), &bboxes[i + 1],
+                                        boundary, boundaryBBox))) {
             return false;
         }
     }
