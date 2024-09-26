fa3b1b8040c34255ab67541ea9bae5952b34d2dd
AJ Friend <ajfriend@gmail.com>
2024-09-26 11:42:42 -0700

    Demonstrate failure at numUncompacted = 41

--- src/apps/testapps/testCompactCells.c
+++ src/apps/testapps/testCompactCells.c
@@ -109,7 +109,7 @@ SUITE(compactCells) {
         // However:
         //   Passes if numUncompacted <= 40
         //   Fails  if numUncompacted >= 41.
-        int64_t numUncompacted = 40;
+        int64_t numUncompacted = 41;
         t_assertSuccess(H3_EXPORT(compactCells)(cells1, out, numUncompacted));
 
         // TODO: check that output matches cells0
