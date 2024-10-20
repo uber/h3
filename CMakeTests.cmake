1e275c949c1d3c6757c45b752ccb227a1e2c10c2
Isaac Brodsky <isaac@isaacbrodsky.com>
2024-10-20 09:22:09 -0700

    tab size 4

--- CMakeTests.cmake
+++ CMakeTests.cmake
@@ -25,173 +25,178 @@ enable_testing()
 set(test_number 0)
 
 if(ENABLE_COVERAGE)
-  file(
-    GENERATE
-    OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/scripts/coverage.sh"
-    INPUT "${CMAKE_CURRENT_SOURCE_DIR}/scripts/coverage.sh.in")
-  set_property(
-    DIRECTORY
-    APPEND
-    PROPERTY ADDITIONAL_MAKE_CLEAN_FILES "coverage.info")
-  set_property(
-    DIRECTORY
-    APPEND
-    PROPERTY ADDITIONAL_MAKE_CLEAN_FILES "coverage.cleaned.info")
-  set_property(
-    DIRECTORY
-    APPEND
-    PROPERTY ADDITIONAL_MAKE_CLEAN_FILES "coverage")
-  add_custom_target(
-    coverage
-    COMMAND bash "${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/scripts/coverage.sh"
+    file(
+        GENERATE
+        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/scripts/coverage.sh"
+        INPUT "${CMAKE_CURRENT_SOURCE_DIR}/scripts/coverage.sh.in")
+    set_property(
+        DIRECTORY
+        APPEND
+        PROPERTY ADDITIONAL_MAKE_CLEAN_FILES "coverage.info")
+    set_property(
+        DIRECTORY
+        APPEND
+        PROPERTY ADDITIONAL_MAKE_CLEAN_FILES "coverage.cleaned.info")
+    set_property(
+        DIRECTORY
+        APPEND
+        PROPERTY ADDITIONAL_MAKE_CLEAN_FILES "coverage")
+    add_custom_target(
+        coverage
+        COMMAND
+            bash "${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/scripts/coverage.sh"
             "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}")
-  add_custom_target(
-    clean-coverage
-    # Before running coverage, clear all counters
-    COMMAND lcov --rc lcov_branch_coverage=1 --directory
-            '${CMAKE_CURRENT_BINARY_DIR}' --zerocounters
-    COMMENT "Zeroing counters")
+    add_custom_target(
+        clean-coverage
+        # Before running coverage, clear all counters
+        COMMAND lcov --rc lcov_branch_coverage=1 --directory
+                '${CMAKE_CURRENT_BINARY_DIR}' --zerocounters
+        COMMENT "Zeroing counters")
 endif()
 
 macro(add_h3_memory_test name srcfile)
-  # Like other test code, but these need to be linked against a different copy
-  # of the H3 library which has known intercepted allocator functions.
-  add_executable(${ARGV} ${APP_SOURCE_FILES} ${TEST_APP_SOURCE_FILES})
+    # Like other test code, but these need to be linked against a different copy
+    # of the H3 library which has known intercepted allocator functions.
+    add_executable(${ARGV} ${APP_SOURCE_FILES} ${TEST_APP_SOURCE_FILES})
 
-  if(TARGET ${name})
-    target_link_libraries(${name} PUBLIC h3WithTestAllocators)
-    target_include_directories(
-      ${name}
-      PUBLIC
-        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src/apps/applib/include>)
-    target_compile_options(${name} PRIVATE ${H3_COMPILE_FLAGS})
-    target_link_libraries(${name} PRIVATE ${H3_LINK_FLAGS})
-  endif()
+    if(TARGET ${name})
+        target_link_libraries(${name} PUBLIC h3WithTestAllocators)
+        target_include_directories(
+            ${name}
+            PUBLIC
+                $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src/apps/applib/include>
+        )
+        target_compile_options(${name} PRIVATE ${H3_COMPILE_FLAGS})
+        target_link_libraries(${name} PRIVATE ${H3_LINK_FLAGS})
+    endif()
 
-  math(EXPR test_number "${test_number}+1")
+    math(EXPR test_number "${test_number}+1")
 
-  add_test(NAME ${name}_test${test_number} COMMAND ${TEST_WRAPPER}
-                                                   "$<TARGET_FILE:${name}>")
+    add_test(NAME ${name}_test${test_number} COMMAND ${TEST_WRAPPER}
+                                                     "$<TARGET_FILE:${name}>")
 
-  if(ENABLE_COVERAGE)
-    add_custom_target(
-      ${name}_coverage${test_number}
-      COMMAND ${name} > /dev/null
-      COMMENT "Running ${name}_coverage${test_number}")
+    if(ENABLE_COVERAGE)
+        add_custom_target(
+            ${name}_coverage${test_number}
+            COMMAND ${name} > /dev/null
+            COMMENT "Running ${name}_coverage${test_number}")
 
-    add_dependencies(coverage ${name}_coverage${test_number})
-    add_dependencies(${name}_coverage${test_number} clean-coverage)
-  endif()
+        add_dependencies(coverage ${name}_coverage${test_number})
+        add_dependencies(${name}_coverage${test_number} clean-coverage)
+    endif()
 endmacro()
 
 macro(add_h3_test_common name srcfile)
-  # need to actually make the test target
-  if(NOT TARGET ${name})
-    add_h3_executable(${name} ${srcfile} ${APP_SOURCE_FILES}
-                      ${TEST_APP_SOURCE_FILES})
-  endif()
+    # need to actually make the test target
+    if(NOT TARGET ${name})
+        add_h3_executable(${name} ${srcfile} ${APP_SOURCE_FILES}
+                          ${TEST_APP_SOURCE_FILES})
+    endif()
 
-  math(EXPR test_number "${test_number}+1")
+    math(EXPR test_number "${test_number}+1")
 endmacro()
 
 macro(add_h3_test name srcfile)
-  add_h3_test_common(${name} ${srcfile})
-  add_test(NAME ${name}_test${test_number} COMMAND ${TEST_WRAPPER}
-                                                   "$<TARGET_FILE:${name}>")
+    add_h3_test_common(${name} ${srcfile})
+    add_test(NAME ${name}_test${test_number} COMMAND ${TEST_WRAPPER}
+                                                     "$<TARGET_FILE:${name}>")
 
-  if(ENABLE_COVERAGE)
-    add_custom_target(
-      ${name}_coverage${test_number}
-      COMMAND ${name} > /dev/null
-      COMMENT "Running ${name}_coverage${test_number}")
+    if(ENABLE_COVERAGE)
+        add_custom_target(
+            ${name}_coverage${test_number}
+            COMMAND ${name} > /dev/null
+            COMMENT "Running ${name}_coverage${test_number}")
 
-    add_dependencies(coverage ${name}_coverage${test_number})
-    add_dependencies(${name}_coverage${test_number} clean-coverage)
-  endif()
+        add_dependencies(coverage ${name}_coverage${test_number})
+        add_dependencies(${name}_coverage${test_number} clean-coverage)
+    endif()
 endmacro()
 
 macro(add_h3_test_with_file name srcfile argfile)
-  add_h3_test_common(${name} ${srcfile})
-  # add a special command (so we don't need to read the test file from the test
-  # program)
-  set(dump_command "cat")
+    add_h3_test_common(${name} ${srcfile})
+    # add a special command (so we don't need to read the test file from the
+    # test program)
+    set(dump_command "cat")
 
-  add_test(
-    NAME ${name}_test${test_number}
-    COMMAND
-      ${SHELL}
-      "${dump_command} ${argfile} | ${TEST_WRAPPER_STR} $<TARGET_FILE:${name}>")
+    add_test(
+        NAME ${name}_test${test_number}
+        COMMAND
+            ${SHELL}
+            "${dump_command} ${argfile} | ${TEST_WRAPPER_STR} $<TARGET_FILE:${name}>"
+    )
 
-  if(PRINT_TEST_FILES)
-    message("${name}_test${test_number} - ${argfile}")
-  endif()
+    if(PRINT_TEST_FILES)
+        message("${name}_test${test_number} - ${argfile}")
+    endif()
 
-  if(ENABLE_COVERAGE)
-    add_custom_target(
-      ${name}_coverage${test_number}
-      COMMAND ${name} < ${argfile} > /dev/null
-      COMMENT "Running ${name}_coverage${test_number}")
+    if(ENABLE_COVERAGE)
+        add_custom_target(
+            ${name}_coverage${test_number}
+            COMMAND ${name} < ${argfile} > /dev/null
+            COMMENT "Running ${name}_coverage${test_number}")
 
-    add_dependencies(coverage ${name}_coverage${test_number})
-    add_dependencies(${name}_coverage${test_number} clean-coverage)
-  endif()
+        add_dependencies(coverage ${name}_coverage${test_number})
+        add_dependencies(${name}_coverage${test_number} clean-coverage)
+    endif()
 endmacro()
 
 macro(add_h3_cli_test name h3_args expect_string)
-  add_test(
-    NAME ${name}_test${test_number}
-    COMMAND ${SHELL}
+    add_test(
+        NAME ${name}_test${test_number}
+        COMMAND
+            ${SHELL}
             "test \"`$<TARGET_FILE:h3_bin> ${h3_args}`\" = '${expect_string}'")
 
-  if(PRINT_TEST_FILES)
-    message("${name}_test${test_number} - ${h3_args} - ${expect_string}")
-  endif()
+    if(PRINT_TEST_FILES)
+        message("${name}_test${test_number} - ${h3_args} - ${expect_string}")
+    endif()
 
-  # TODO: Build a coverage-enabled variant of the h3 cli app to enable coverage
+    # TODO: Build a coverage-enabled variant of the h3 cli app to enable
+    # coverage
 endmacro()
 
 macro(add_h3_test_with_arg name srcfile arg)
-  add_h3_test_common(${name} ${srcfile})
-  add_test(NAME ${name}_test${test_number}
-           COMMAND ${TEST_WRAPPER} $<TARGET_FILE:${name}> ${arg})
-  if(PRINT_TEST_FILES)
-    message("${name}_test${test_number} - ${arg}")
-  endif()
+    add_h3_test_common(${name} ${srcfile})
+    add_test(NAME ${name}_test${test_number}
+             COMMAND ${TEST_WRAPPER} $<TARGET_FILE:${name}> ${arg})
+    if(PRINT_TEST_FILES)
+        message("${name}_test${test_number} - ${arg}")
+    endif()
 
-  if(ENABLE_COVERAGE)
-    add_custom_target(
-      ${name}_coverage${test_number}
-      COMMAND ${name} ${arg}
-      COMMENT "Running ${name}_coverage${test_number}")
+    if(ENABLE_COVERAGE)
+        add_custom_target(
+            ${name}_coverage${test_number}
+            COMMAND ${name} ${arg}
+            COMMENT "Running ${name}_coverage${test_number}")
 
-    add_dependencies(coverage ${name}_coverage${test_number})
-    add_dependencies(${name}_coverage${test_number} clean-coverage)
-  endif()
+        add_dependencies(coverage ${name}_coverage${test_number})
+        add_dependencies(${name}_coverage${test_number} clean-coverage)
+    endif()
 endmacro()
 
 # Add each individual test
 file(GLOB all_centers tests/inputfiles/bc*centers.txt)
 foreach(file ${all_centers})
-  add_h3_test_with_file(testLatLngToCell src/apps/testapps/testLatLngToCell.c
-                        ${file})
+    add_h3_test_with_file(testLatLngToCell src/apps/testapps/testLatLngToCell.c
+                          ${file})
 endforeach()
 
 file(GLOB all_ic_files tests/inputfiles/res*ic.txt)
 foreach(file ${all_ic_files})
-  add_h3_test_with_file(testCellToLatLng src/apps/testapps/testCellToLatLng.c
-                        ${file})
+    add_h3_test_with_file(testCellToLatLng src/apps/testapps/testCellToLatLng.c
+                          ${file})
 endforeach()
 
 file(GLOB all_centers tests/inputfiles/rand*centers.txt)
 foreach(file ${all_centers})
-  add_h3_test_with_file(testLatLngToCell src/apps/testapps/testLatLngToCell.c
-                        ${file})
+    add_h3_test_with_file(testLatLngToCell src/apps/testapps/testLatLngToCell.c
+                          ${file})
 endforeach()
 
 file(GLOB all_cells tests/inputfiles/*cells.txt)
 foreach(file ${all_cells})
-  add_h3_test_with_file(testCellToBoundary
-                        src/apps/testapps/testCellToBoundary.c ${file})
+    add_h3_test_with_file(testCellToBoundary
+                          src/apps/testapps/testCellToBoundary.c ${file})
 endforeach()
 
 add_h3_test(testCellToBoundaryEdgeCases
@@ -277,13 +282,13 @@ add_h3_test(testCellToBBoxExhaustive
 
 file(GLOB cli_tests tests/cli/*.txt)
 foreach(file ${cli_tests})
-  include(${file})
+    include(${file})
 endforeach()
 
 if(BUILD_ALLOC_TESTS)
-  add_h3_library(h3WithTestAllocators test_prefix_)
+    add_h3_library(h3WithTestAllocators test_prefix_)
 
-  add_h3_memory_test(testH3Memory src/apps/testapps/testH3Memory.c)
+    add_h3_memory_test(testH3Memory src/apps/testapps/testH3Memory.c)
 endif()
 
 add_custom_target(test-fast COMMAND ctest -E Exhaustive)
--- cmake/TestWrapValgrind.cmake
+++ cmake/TestWrapValgrind.cmake
@@ -15,7 +15,7 @@
 # Support code for wrapping tests or other executables with Valgrind.
 
 if(__test_wrap_valgrind)
-  return()
+    return()
 endif()
 set(__test_wrap_valgrind 1)
 
@@ -25,13 +25,13 @@ find_program(VALGRIND valgrind)
 cmake_dependent_option(WRAP_VALGRIND "Wrap tests in valgrind" OFF "VALGRIND"
                        OFF)
 if(WRAP_VALGRIND)
-  # TEST_WRAPPER could be used to configure the Valgrind parameters, or to use a
-  # different wrapper entirely.
-  set(TEST_WRAPPER
-      ${VALGRIND} --track-origins=yes --leak-check=full --error-exitcode=99
-      CACHE STRING "Wrapper executable for tests and benchmarks")
-  mark_as_advanced(TEST_WRAPPER)
-  # Convert from semicolon separated list of values to a form that can be used
-  # by a shell.
-  string(REPLACE ";" " " TEST_WRAPPER_STR "${TEST_WRAPPER}")
+    # TEST_WRAPPER could be used to configure the Valgrind parameters, or to use
+    # a different wrapper entirely.
+    set(TEST_WRAPPER
+        ${VALGRIND} --track-origins=yes --leak-check=full --error-exitcode=99
+        CACHE STRING "Wrapper executable for tests and benchmarks")
+    mark_as_advanced(TEST_WRAPPER)
+    # Convert from semicolon separated list of values to a form that can be used
+    # by a shell.
+    string(REPLACE ";" " " TEST_WRAPPER_STR "${TEST_WRAPPER}")
 endif()
