7db44e1c6e97c0aba1105675ea5a88bca018e216
Isaac Brodsky <isaac@isaacbrodsky.com>
2025-05-01 09:31:35 -0700

    add SQL docs

--- website/src/components/explorer/map.jsx
+++ website/src/components/explorer/map.jsx
@@ -15,6 +15,7 @@ import { getRes0Cells, uncompactCells, cellToBoundary } from "h3-js";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import { MOBILE_CUTOFF_WINDOW_WIDTH } from "../common";
 import { useHex } from "./useHex";
+import {FullscreenWidget} from "@deck.gl/widgets";
 
 const INITIAL_VIEW_STATE = {
   longitude: -74.012,
@@ -205,6 +206,12 @@ export function ExplorerMap({
         dragPan: windowWidth && windowWidth >= MOBILE_CUTOFF_WINDOW_WIDTH,
         dragRotate: false,
       }}
+      widgets={[
+        new FullscreenWidget({
+          id: "fullscreen",
+          
+        })
+      ]}
       touchAction="pan-y"
     >
       <Map
