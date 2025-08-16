73e99857be29a1a088ca9386ca523c72db9a92ff
AJ Friend <ajfriend@gmail.com>
2025-08-16 11:41:32 -0700

    clean up

--- src/apps/testapps/testCreateCell.c
+++ src/apps/testapps/testCreateCell.c
@@ -20,62 +20,51 @@
  */
 
 #include <math.h>
-#include <string.h>
 
-#include "h3Index.h"
+#include "h3Index.h"  // can i get rid of this and just pull in the API
 #include "h3api.h"
 #include "latLng.h"
 #include "test.h"
 #include "utility.h"
 
 typedef struct {
-    int res;
-    int bc;
-    int digits[15];
-} CellComponents;
-
-typedef struct {
-    int res;
-    int bc;
-    int digits[15];
-    H3Index h;  // expected output
-} ComponentTest;
-
-H3Index components_to_cell(CellComponents cc) {
     H3Index h;
+    int res;
+    int bc;
+    int digits[15];
+} CellAndComponents;
 
-    H3_EXPORT(createCell)(cc.res, cc.bc, cc.digits, &h);
-
+H3Index components_to_cell(CellAndComponents cnc) {
+    H3Index h;
+    H3_EXPORT(createCell)(cnc.res, cnc.bc, cnc.digits, &h);
     return h;
 }
 
-CellComponents cell_to_components(H3Index h) {
-    CellComponents cc;
+CellAndComponents cell_to_components(H3Index h) {
+    CellAndComponents cnc = {.h = h,
+                             .res = H3_EXPORT(getResolution)(h),
+                             .bc = H3_EXPORT(getBaseCellNumber)(h)};
 
-    cc.res = H3_EXPORT(getResolution)(h);
-    cc.bc = H3_EXPORT(getBaseCellNumber)(h);
-
-    for (int r = 1; r <= cc.res; r++) {
-        H3_EXPORT(getIndexDigit)(h, r, &cc.digits[r - 1]);
+    for (int r = 1; r <= cnc.res; r++) {
+        H3_EXPORT(getIndexDigit)(h, r, &cnc.digits[r - 1]);
     }
 
-    return cc;
+    return cnc;
 }
 
-void do_component_test(ComponentTest ct) {
-    CellComponents cc = {.res = ct.res, .bc = ct.bc};
-    memcpy(cc.digits, ct.digits, sizeof cc.digits);
+void validate_cnc(CellAndComponents a) {
+    H3Index h = components_to_cell(a);
+    t_assert(h == a.h, "match");
+    t_assert(H3_EXPORT(isValidCell)(h), "should be valid cell");
 
-    H3Index out = components_to_cell(cc);
-    t_assert(out == ct.h, "match");
-    t_assert(H3_EXPORT(isValidCell)(out), "should be valid cell");
+    CellAndComponents b = cell_to_components(a.h);
 
-    CellComponents cc2 = cell_to_components(ct.h);
+    t_assert(a.h == b.h, "bah");
+    t_assert(a.res == b.res, "bah");
+    t_assert(a.bc == b.bc, "bah");
 
-    t_assert(cc.res == cc2.res, "bah");
-    t_assert(cc.bc == cc2.bc, "bah");
-    for (int r = 1; r <= cc.res; r++) {
-        t_assert(cc.digits[r - 1] == cc.digits[r - 1], "bah");
+    for (int r = 1; r <= a.res; r++) {
+        t_assert(a.digits[r - 1] == b.digits[r - 1], "bah");
     }
 }
 
@@ -116,15 +105,7 @@ SUITE(createCell) {
     }
 
     TEST(createCellFancy) {
-        CellComponents cc = {.res = 3, .bc = 73, .digits = {1, 2, 3}};
-
-        H3Index h = components_to_cell(cc);
-        t_assert(h == 0x839253fffffffff, "match");
-        t_assert(H3_EXPORT(isValidCell)(h), "should be valid cell");
-    }
-
-    TEST(createCellFancy2) {
-        ComponentTest tests[] = {
+        CellAndComponents tests[] = {
             {.h = 0x8001fffffffffff, .res = 0, .bc = 0, .digits = {}},
             {.h = 0x8003fffffffffff, .res = 0, .bc = 1, .digits = {}},
             {.h = 0x80f3fffffffffff, .res = 0, .bc = 121, .digits = {}},
@@ -133,8 +114,8 @@ SUITE(createCell) {
             {.h = 0x821f67fffffffff, .res = 2, .bc = 15, .digits = {5, 4}},
             {.h = 0x8155bffffffffff, .res = 1, .bc = 42, .digits = {6}}};
 
-        for (int i = 0; i < sizeof tests / sizeof tests[0]; i++) {
-            do_component_test(tests[i]);
+        for (int i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {
+            validate_cnc(tests[i]);
         }
         // TODO: can have some examples expect a certain error. then check for
         // those!
