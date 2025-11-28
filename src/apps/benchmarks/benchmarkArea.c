c915b5183011839bb211a305241454134ee8c759
AJ Friend <ajfriend@gmail.com>
2025-11-27 22:11:55 -0500

    turn it to 11.

--- src/apps/benchmarks/benchmarkArea.c
+++ src/apps/benchmarks/benchmarkArea.c
@@ -38,7 +38,7 @@ static void do_res_sum(int res) {
 BEGIN_BENCHMARKS();
 
 BENCHMARK(directedEdgeToBoundary, 1, {
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i <= 8; i++) {
         do_res_sum(i);
     }
 });
