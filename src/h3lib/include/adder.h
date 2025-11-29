4a4fb073b4eebcd9237a0489e8fea1b717ccee3b
AJ Friend <ajfriend@gmail.com>
2025-11-28 22:48:05 -0500

    adder docs

--- src/h3lib/include/adder.h
+++ src/h3lib/include/adder.h
@@ -2,15 +2,16 @@
 #define ADDER_H
 
 /*
-This is a header-only implementation of a "compensated summation" algorithm,
-which allows us to add up sequences of floating point numbers with better
-precision than naive summation, especially when the terms in the sum
-can vary in magnitude.
+Header-only implementation of a "compensated summation" algorithm (Kahan
+summation), which allows us to add up sequences of floating-point numbers
+with better precision than naive summation, especially when the terms in
+the sum vary significantly in magnitude.
 See: https://en.wikipedia.org/wiki/Kahan_summation_algorithm
 
-This is useful when computing the area of (multi-)polygons, which can involve
-adding many small terms to large aggregates.
-For example, D3 uses this when computing polygonal areas via the `Kahan` class:
+This is useful when computing the area of (multi)polygons, which
+often involves adding many small terms to a large aggregate. For example,
+D3 uses an improved accuracy summation when computing polygonal areas via its
+`Adder` class:
 https://github.com/d3/d3-geo/blob/main/src/area.js
 
 There are a few potential algorithms we might consider for summation:
@@ -18,14 +19,14 @@ There are a few potential algorithms we might consider for summation:
 1. Naive sum
 2. Kahan summation
 3. Neumaier summation
-4. Other approaches like pairwise summation, or Pythons `fsum`
+4. Other approaches like pairwise summation, or Python's `fsum`
 
-We considered the first three for simplicity, and settled on Kahan summation,
-as it acheives noticably better accuracy than naive summation and almost as good
-accuracy as Neumaier, while only being slightly slower than naive, but slightly
-faster and simpler than Neumaier.
+We considered the first three for simplicity, and settled on Kahan summation:
+it achieves noticeably better accuracy than naive summation and almost as
+good accuracy as Neumaier, while being only slightly slower than naive and
+slightly faster and simpler than Neumaier.
 
-See also: https://github.com/python/cpython/issues/100425 for discussions of
+See also: https://github.com/python/cpython/issues/100425 for discussion of
 tradeoffs between Kahan, Neumaier, and `fsum`.
 */
 
@@ -62,15 +63,15 @@ static inline double neumaier_result(Adder adder) {
 }
 
 static inline void kadd(Adder *adder, double x) {
-    // simple_add(adder, x);
+    simple_add(adder, x);
     // kahan_add(adder, x);
-    neumaier_add(adder, x);
+    // neumaier_add(adder, x);
 }
 
 static inline double kresult(Adder adder) {
-    // return simple_result(adder);
+    return simple_result(adder);
     // return kahan_result(adder);
-    return neumaier_result(adder);
+    // return neumaier_result(adder);
 }
 
 #endif
