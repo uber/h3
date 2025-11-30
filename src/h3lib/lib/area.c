06b2a6b8b6d9348311483d296b5af740d584623a
AJ Friend <ajfriend@gmail.com>
2025-11-29 23:08:53 -0500

    better docstrings

--- src/h3lib/lib/area.c
+++ src/h3lib/lib/area.c
@@ -11,8 +11,11 @@
 #include "constants.h"
 #include "h3api.h"
 
+
+// Cagnoli contribution for edge arc x to y, following d3-geo’s
+// area implementation:
+// https://github.com/d3/d3-geo/blob/8c53a90ae70c94bace73ecb02f2c792c649c86ba/src/area.js#L51-L70
 static inline double cagnoli(LatLng x, LatLng y) {
-    // https://github.com/d3/d3-geo/blob/8c53a90ae70c94bace73ecb02f2c792c649c86ba/src/area.js#L51-L70
     x.lat = x.lat / 2.0 + M_PI / 4.0;
     y.lat = y.lat / 2.0 + M_PI / 4.0;
 
@@ -31,20 +34,30 @@ static inline double cagnoli(LatLng x, LatLng y) {
  *
  * The GeoLoop should represent a simple curve with no self-intersections.
  * Vertices should be ordered according to the "right hand rule".
- * That is, if you are looking from outer space at a spherical polygon
+ * That is, if you are looking from outer space at a spherical polygon loop
  * on the surface of the earth whose interior is contained within a hemisphere,
  * then the vertices should be ordered counter-clockwise. The interior of the
- * polygon is to the left of a person walking along the boundary of the polygon
+ * loop is to the left of a person walking along the boundary of the polygon
  * in the counter-clockwise direction.
  *
  * Note that GeoLoops do not need to repeat the first vertex at the end of the
  * array to close the loop; this is done automatically.
  *
+ * The edge arcs between adjacent vertices are assumed to be the shortest
+ * geodesic path between them; that is, all arcs are interpreted to be less
+ * than 180 degrees or pi radians.
+ * Avoid arcs that are exactly pi (i.e, two antipodal vertices).
+ * "Large" polygon loops (e.g., that cannot be contained in a hemisphere) can
+ * still be constructed by using intermediate vertices with arcs less than
+ * 180 degrees, and the loop area will still be computed correctly.
+ *
  * The area of the entire globe is 4*pi radians^2. If, for example, you have a
  * small GeoLoop with area `a << 4*pi` and then reverse the order of the
- * vertices, you produce GeoLoop with area `4*pi - a`, since by the right hand
- * rule, the interior of the new loop's interior is the majority of the globe,
+ * vertices, you produce GeoLoop with area `4*pi - a`, since, by the right hand
+ * rule, the new loop's interior is the majority of the globe,
  * or "everything except the original polygon".
+ * Note that the area enclosed by the loop is determined by the vertex order;
+ * this function does **not** return `min(a, 4*pi - a)`.
  *
  * @param   loop  GeoLoop of boundary vertices in counter-clockwise order
  * @param    out  loop area in radians^2, in interval [0, 4*pi]
@@ -58,6 +71,10 @@ H3Error H3_EXPORT(geoLoopArea)(GeoLoop loop, double *out) {
         kadd(&adder, cagnoli(loop.verts[i], loop.verts[j]));
     }
 
+    // The Cagnoli sum above yields a signed area, with the sign switching
+    // with the orientation of the vertices. Since we want our area to always be
+    // positive, we normalize into [0, 4*pi] by adding 4*pi when the signed
+    // area is negative.
     if (adder.sum < 0.0) {
         kadd(&adder, 4.0 * M_PI);
     }
