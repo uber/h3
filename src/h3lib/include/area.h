77278a6089e1a83222c317fc78a87b0735ace5d6
AJ Friend <ajfriend@gmail.com>
2025-12-01 18:50:26 -0500

    clear these comments, maybe?

--- src/h3lib/include/area.h
+++ src/h3lib/include/area.h
@@ -3,14 +3,6 @@
 
 #include "h3api.h"
 
-/** @defgroup geoLoopAreaRads2 geoLoopAreaRads2
- * Functions for geoLoopAreaRads2
- * @{
- */
-/**
- * @brief Area in radians^2 enclosed by vertices of a GeoLoop.
- */
 H3Error geoLoopAreaRads2(GeoLoop loop, double *out);
-/** @} */
 
 #endif
